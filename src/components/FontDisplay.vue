d6a5f5ee4b05cfcc2097bb37ade092783c5e9a1f
Javier Reyes <javier.reys@gmail.com>
2022-11-03 20:15:13 -0300

    feat: add buttons to update the font

--- src/components/FontDisplay.vue
+++ src/components/FontDisplay.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, markRaw, watch } from "vue";
 import { loadFont } from "@/helpers/fontkit-utils";
+import { getBase64, injectGlobalFontFace } from "@/helpers/css-utils";
 import type { Font } from "fontkit";
 
 interface InputFileEvent extends Event {
@@ -8,24 +9,54 @@ interface InputFileEvent extends Event {
 }
 
 const font = ref<Font | null>(null);
+const url = ref<string>("");
 
 const setFont = async (e: InputFileEvent) => {
   const fontFile = e.target.files && e.target.files[0];
   if (!fontFile) return;
 
+  url.value = await getBase64(fontFile);
   const fontData = await loadFont(fontFile);
-  font.value = fontData;
+  font.value = markRaw(fontData);
 };
+
+watch(font, (font) => {
+  if (!font) return;
+  const fontStyle = font["OS/2"].fsSelection.italic ? "italic" : "normal";
+  injectGlobalFontFace({
+    fontFamily: font.familyName,
+    fontStyle,
+    fontWeight: 500,
+    url: url.value,
+  });
+});
 </script>
 
 <template>
-  <div class="mt-10">
-    <h1 class="text-3xl font-semibold tracking-tight text-stone-900">
-      {{ font?.fullName }}
-    </h1>
-    <span class="text-stone-500 text-sm">
-      {{ font?.copyright }}
-    </span>
+  <div class="mt-10 flex justify-between items-center">
+    <div>
+      <h1 class="text-3xl font-semibold tracking-tight text-stone-900">
+        {{ font?.fullName }}
+      </h1>
+      <span class="text-stone-500 text-sm">
+        {{ font?.copyright }}
+      </span>
+    </div>
+    <label
+      v-if="font"
+      for="update-font"
+      class="bg-blue-500 text-sm hover:bg-blue-600 rounded-lg px-4 py-2 text-white font-semibold cursor-pointer"
+    >
+      <input
+        id="update-font"
+        name="update-font"
+        type="file"
+        @change.prevent="setFont"
+        class="sr-only"
+        accept=".ttf,.otf,.woff"
+      />
+      Upload new font
+    </label>
   </div>
   <div
     v-if="!font"
@@ -53,5 +84,15 @@ const setFont = async (e: InputFileEvent) => {
       <p class="text-xs text-slate-500">TTF, OTF, WOFF up to 10MB</p>
     </div>
   </div>
-  <!-- @change="setFont" -->
+
+  <div
+    contenteditable
+    spellcheck="false"
+    class="text-[3rem]"
+    :style="{ fontFamily: font?.familyName }"
+  >
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores fuga
+    asperiores distinctio aliquid placeat voluptate eaque? Maiores beatae maxime
+    eligendi, ad magnam ullam voluptatem atque eaque reprehenderit modi amet ea.
+  </div>
 </template>
